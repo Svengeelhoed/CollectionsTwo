7935889ceba5846439fb8afd83716500c367b564
Svengeelhoed <99069588@mydavinci.nl>
2021-12-13 12:09:09 +0100

    kaarten generator af

---
+++ deck.py
@@ -0,0 +1,11 @@
+import random
+deck = ["harten 2","harten 3","harten 4","harten 5","harten 6","harten 7","harten 8","harten 9","harten 10","harten boer","harten vrouw","harten koning", "harten aas", "schoppen aas", "schoppen 2","schoppen 3","schoppen 4","schoppen 5","schoppen 6","schoppen 7","schoppen 8","schoppen 9","schoppen 10","schoppen boer","schoppen vrouw","schoppen koning","ruiten aas","ruiten 2","ruiten 3","ruiten 4","ruiten 5","ruiten 6","ruiten 7","ruiten 8","ruiten 9","ruiten 10","ruiten boer","ruiten vrouw","ruiten koning","klaver aas","klaver 2","klaver 3","klaver 4","klaver 5","klaver 6","klaver 7","klaver 8","klaver 9","klaver 10","klaver boer","klaver vrouw","klaver koning"]
+print("kaart 1: ", random.choice(deck))
+print("kaart 2: ", random.choice(deck))
+print("kaart 3: ", random.choice(deck))
+print("kaart 4: ", random.choice(deck))
+print("kaart 5: ", random.choice(deck))
+print("kaart 6: ", random.choice(deck))
+print("kaart 7: ", random.choice(deck))
+print("deck (47 kaarten: ", deck)
+
